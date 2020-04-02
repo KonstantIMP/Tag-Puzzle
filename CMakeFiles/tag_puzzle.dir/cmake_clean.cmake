934486cfe2dbd0b76bcb43b11cc0cd40dabc5714
KonstantinProgger <mihedovkos@gmail.com>
2020-04-02 16:19:35 +0300

    Доделал cmake

---
+++ CMakeFiles/tag_puzzle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tag_puzzle.dir/src/main.cpp.o"
+  "CMakeFiles/tag_puzzle.dir/src/tag_puzzle.cpp.o"
+  "tag_puzzle"
+  "tag_puzzle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tag_puzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
